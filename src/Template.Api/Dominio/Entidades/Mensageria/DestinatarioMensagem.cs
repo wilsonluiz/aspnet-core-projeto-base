2b00980628449cfabc3136d9aab431a881fc7354
Wilson I Luiz <wilson@t2s.com.br>
2019-01-30 23:59:55 -0200

    Adicionadas propredades da entidade Destinatario da Mensagem

--- src/Template.Api/Dominio/Entidades/Mensageria/DestinatarioMensagem.cs
+++ src/Template.Api/Dominio/Entidades/Mensageria/DestinatarioMensagem.cs
@@ -1,13 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Template.Api.Dominio.Entidades.ControleAcesso;
 
 namespace Template.Api.Dominio.Entidades.Mensageria
 {
-    public class DestinatarioMensagem
+    public class DestinatarioMensagem : EntidadeBase
     {
-        
+        public int IdMensagem { get; set; }
+        public int IdUsuarioDestinatario { get; set; }
+        public string Conteudo { get; set; }
+        public DateTime DataLeitura { get; set; }
+        public DateTime DataArquivamento { get; set; }
+
+        public Mensagem Mensagem { get; set; }
+        public Usuario Usuario { get; set; }
     }
 }
