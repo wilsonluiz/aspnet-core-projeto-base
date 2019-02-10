862b2a6a3c13fd687e219b42890dccf638dc2e80
Wilson I Luiz <wilson@t2s.com.br>
2019-02-10 17:54:54 -0200

    Criado controller e view base da aplicação

---
+++ src/Template.Api/Controllers/AppController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Template.Api.Controllers
+{
+    [Route("app")]
+    public class AppController : Controller
+    {
+        [HttpGet("index")]
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
