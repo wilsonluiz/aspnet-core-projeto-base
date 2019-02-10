c8a65f17ae650f05e12cc2c3989669aff688ba26
Wilson I Luiz <wilson@t2s.com.br>
2019-02-10 15:27:54 -0200

    Alterada classe base do Usuário para implementação do ASP.NET Core Identity. Removida propriedades duplicadas

--- src/Template.Api/Dominio/Entidades/ControleAcesso/Usuario.cs
+++ src/Template.Api/Dominio/Entidades/ControleAcesso/Usuario.cs
@@ -1,14 +1,14 @@
 ﻿using System;
+using Microsoft.AspNetCore.Identity;
 
 namespace Template.Api.Dominio.Entidades.ControleAcesso
 {
-    public class Usuario : EntidadeBase
+    public class Usuario : IdentityUser<int>
     {
+        public override int Id { get; set; }
         public string Nome { get; set; }
         public string Matricula { get; set; }
         public string Cpf { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
         public DateTime DataNascimento { get; set; }
         public DateTime? DataExclusao { get; set; }
     }
